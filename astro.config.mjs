5cdd164786d8577d938f06ea86bfc4b8e3cfc3c1
Yuki Okawa <0k4w4.yuk1@gmail.com>
2025-08-12 22:44:34 +0900

    Added Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ykokw.com'
+  site: 'https://ykokw.com',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
