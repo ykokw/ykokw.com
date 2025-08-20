1d1fa6e52203786773769c7f83a3e8407e1e587c
Yuki Okawa <0k4w4.yuk1@gmail.com>
2025-08-20 23:11:10 +0900

    Add rehype plugin to set target and rel attribute to markdown link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import rehypeExternalLinks from "rehype-external-links";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -8,5 +9,26 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: "noreferrer",
+          content: {
+            type: "text",
+            value: "open_in_new",
+          },
+          contentProperties: {
+            className:
+              "material-symbols-outlined inline-flex align-middle text-sm! text-blue-white opacity-70 ml-1",
+            "aria-label": "新しいタブで開く",
+          },
+        },
+      ],
+    ],
   },
 });
