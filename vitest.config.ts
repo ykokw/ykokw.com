87694a0bfc8000d7327f06c79edac7cbfd4776c0
Yuki Okawa <0k4w4.yuk1@gmail.com>
2025-09-18 06:58:41 -0700

    Added coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { getViteConfig } from 'astro/config';
+import { getViteConfig } from "astro/config";
 
 export default getViteConfig({
   test: {
@@ -7,11 +7,23 @@ export default getViteConfig({
       {
         extends: true,
         test: {
-          include: ['src/libs/**/*.test.ts', 'src/contents/**/*.test.ts'],
-          environment: 'node',
+          include: ["src/libs/**/*.test.ts", "src/contents/**/*.test.ts"],
+          environment: "node",
         },
       },
     ],
-    reporters: process.env.CI ? 'dot' : 'default'
+    reporters: process.env.CI ? "dot" : "default",
+    coverage: {
+      include: [
+        "src/components/*",
+        "src/contents/*",
+        "src/layouts/*",
+        "src/libs/*",
+      ],
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ["text", "json-summary", "json"],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
   },
 });
